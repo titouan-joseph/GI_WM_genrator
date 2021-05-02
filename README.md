a4e8be4844d5ef7b0641890425185e88d2836189
Titouan-Joseph <titouanjoseph@gmail.com>
2021-05-02 15:47:13 +0200

    ➕ Requirements and CLI V1
    ➕ README.md
    ➕ requirements.txt
    ➕ Some basic commands : generate one or multiple name with CLI or txt file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Watermark generator
+
+Take logo on ``assets/img/logo_transparent.png`` with 179px width and 134px height 
+
+Usage of Pillow and argparse for the CLI.  
+To use : 
+```bash
+usage: main.py [-h] [--name NAME] [--multipleName MULTIPLENAME [MULTIPLENAME ...]] [--file FILE] [--verbose]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --name NAME, -n NAME  The name will be write on WM
+  --multipleName MULTIPLENAME [MULTIPLENAME ...], -m MULTIPLENAME [MULTIPLENAME ...]
+                        Generate multiple WM
+  --file FILE, -f FILE  .txt file with names. One per line
+  --verbose, -v         Verbose mode
+
+```
+For example :
+- to generate one WM : ``main.py --name NAME`` or ``main.py -n NAME``
+- to generate multiple WM : ``main.py --multipleName NAME1 NAME2 NAME3`` or ``main.py -m NAME1 NAME2 NAME3``
+- to generate multiple WM base on txt file : ``main.py --file names.txt`` or ``main.py -f names.txt``
--- main.py
+++ main.py
@@ -1,39 +1,76 @@
+#! /usr/bin/python3
 from PIL import Image, ImageDraw, ImageFont
+import argparse
 
-height = 150
-width = 600
 
-# Color
-transparent = (0, 0, 0, 0)
-white = (255, 255, 255)
+def generateWM(name, verbose=False):
+    if verbose:
+        print(f"Creating WM for {name}")
 
-name = "C.Titouan"
+    # Size
+    height = 140
+    width = 690
 
-# Set transparent background
-background = Image.new("RGBA", (width, height), transparent)
+    # Color
+    transparent = (0, 0, 0, 0)
+    white = (255, 255, 255)
 
-# Load logo and font
-logo = Image.open('assets/img/logo_transparent.png')
-font32 = ImageFont.truetype("assets/font/yugothic-medium.otf", 32)
-font72 = ImageFont.truetype("assets/font/yugothic-medium.otf", 72)
+    # Set transparent background
+    background = Image.new("RGBA", (width, height), transparent)
 
-# Rectangle
-rect = Image.new('RGB', (65, 15), white)
+    # Load logo and font
+    logo = Image.open('assets/img/logo_transparent.png')
+    font32 = ImageFont.truetype("assets/font/yugothic-medium.otf", 32)
+    font72 = ImageFont.truetype("assets/font/yugothic-medium.otf", 72)
 
-# Copy logo
-background.paste(logo, (0, 0))
-background.paste(rect, (190, 110))
-background.paste(rect, (525, 110))
+    # Rectangle
+    rect = Image.new('RGB', (110, 15), white)
 
-# Write on WM
-drawer = ImageDraw.Draw(background)
-drawer.text((262, 100), "Graines d'images", font=font32)
-nameSize = font72.getsize(name)
-if nameSize[0] > 410:
-    print("pb avec ce nom, trop grand")
-    exit()
+    # Copy logo & Rectangle
+    background.paste(logo, (0, 0))
+    background.paste(rect, (190, 110))
+    background.paste(rect, (width-110, 110))
 
-drawer.text((190+(205-(nameSize[0]/2)), 20), name, font=font72)
+    # Write "Graines d'images" on WM
+    txt = "Graines d'images"
+    drawer = ImageDraw.Draw(background)
+    drawer.text((190+(250-(font32.getsize(txt)[0]/2)), 100), txt, font=font32)
 
-# Save to png
-background.save(f"WM_{name}.png", "PNG")
+    # Check size of name
+    nameSize = font72.getsize(name)
+    if nameSize[0] > 500:
+        print(nameSize)
+        print(f"The name {name} is to big !")
+        return False
+    # Write name on WM
+    drawer.text((190+(250-(nameSize[0]/2)), 55-(nameSize[1]/2)), name, font=font72)
+
+    # Save to png
+    background.save(f"WM_{name}.png", "PNG")
+    if verbose:
+        print(f"WM created")
+    return True
+
+
+if __name__ == '__main__':
+    # Parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", "-n", help="The name will be write on WM")
+    parser.add_argument("--multipleName", "-m", help="Generate multiple WM", nargs='+')
+    parser.add_argument("--file", "-f", help=".txt file with names. One per line")
+    parser.add_argument("--verbose", "-v", help="Verbose mode", action="store_true")
+
+    # Get arguments
+    args = parser.parse_args()
+
+    if args.name:
+        generateWM(args.name, args.verbose)
+
+    if args.multipleName:
+        for pseudo in args.multipleName:
+            generateWM(pseudo, args.verbose)
+
+    if args.file:
+        with open(args.file, "r") as file:
+            for line in file.readlines():
+                generateWM(line.rsplit('\n')[0], args.verbose)
